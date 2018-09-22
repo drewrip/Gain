c98128710c472d8082a67c1c9991e43ea9f18901
drewrip <drew.ripberger@gmail.com>
2018-09-22 11:52:44 -0400

    Added pages and more file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './src/pages/Home';
+import Studio from './src/pages/Studio';
+import Member from './src/pages/Member'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
+ReactDOM.render(
+  <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    <Route path="/" component={ Home } />
+    <Route path="/studio" component={ Studio } />
+    <Route path="/member" component={ Member } />
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
